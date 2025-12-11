a359d4839e6b4c85014829285e1b83cf53025dd9
Sorry <5646769+sorry1@user.noreply.gitee.com>
2025-12-11 16:59:54 +0800

    feat: 修改类型

--- helper.ts
+++ helper.ts
@@ -1,30 +1,44 @@
-import { AttributeNode, DirectiveNode, ElementNode, findDir, findProp } from '@vue/compiler-core'
-import { MagicString } from '@vue/compiler-sfc'
+import {
+  AttributeNode,
+  DirectiveNode,
+  ElementNode,
+  findDir,
+  findProp
+} from "@vue/compiler-core";
+import { MagicString } from "@vue/compiler-sfc";
 
-import { type BuilderHoistElement } from './builderHoistElement'
+import { type BuilderHoistElement } from "./builderHoistElement";
 const hasPermissionPropsHelps = {
   link() {
-    return 'link'
+    return "link";
   },
   menu() {
-    return `mode="menu"`
+    return `mode="menu"`;
   },
   btn() {
-    return `mode="btn"`
+    return `mode="btn"`;
   }
-} as const
+} as const;
 
-export const needTransformDir = ['if', 'show', 'for', 'else', 'else-if'] as const
-export const needTransformProps = ['key'] as const
+export const needTransformDir = [
+  "if",
+  "show",
+  "for",
+  "else",
+  "else-if"
+] as const;
+export const needTransformProps = ["key"] as const;
 
-function isPermissionProps(modifier: string): modifier is keyof typeof hasPermissionPropsHelps {
-  return (hasPermissionPropsHelps as any)[modifier] !== undefined
+function isPermissionProps(
+  modifier: string
+): modifier is keyof typeof hasPermissionPropsHelps {
+  return (hasPermissionPropsHelps as any)[modifier] !== undefined;
 }
 
 export class BuilderHoistElementContextHelper {
-  private declare bhe: BuilderHoistElement
+  private declare bhe: BuilderHoistElement;
   constructor(bhe: BuilderHoistElement) {
-    this.bhe = bhe
+    this.bhe = bhe;
   }
 
   findNodeDirs() {
@@ -33,7 +47,7 @@ export class BuilderHoistElementContextHelper {
       this.bhe.childMs,
       this.bhe.child,
       findDir
-    )
+    );
   }
   findNodeProps() {
     return this.helpFindNodeProps<AttributeNode>(
@@ -41,33 +55,38 @@ export class BuilderHoistElementContextHelper {
       this.bhe.childMs,
       this.bhe.child,
       findProp
-    )
+    );
   }
   normalizedFallbackSlot(hasPermission: DirectiveNode) {
-    const hasFallBack = hasPermission.modifiers.find((mod) => mod === 'fallback')
+    const hasFallBack = hasPermission.modifiers.find(
+      (mod) => mod.content === "fallback"
+    );
     if (hasFallBack) {
-      return `<template #fallback>${this.getFallBackProp}</template>`
+      return `<template #fallback>${this.getFallBackProp}</template>`;
     }
-    return ''
+    return "";
   }
   get getFallBackProp() {
-    const fallbackProp = findProp(this.bhe.child, 'fallback')
+    const fallbackProp = findProp(this.bhe.child, "fallback");
     if (fallbackProp) {
-      this.bhe.childMs.remove(fallbackProp.loc.start.offset, fallbackProp.loc.end.offset)
+      this.bhe.childMs.remove(
+        fallbackProp.loc.start.offset,
+        fallbackProp.loc.end.offset
+      );
       //静态
       if (fallbackProp?.type === 6) {
-        return fallbackProp.value?.content || ''
+        return fallbackProp.value?.content || "";
       }
       //动态
       if (fallbackProp?.type === 7 && fallbackProp.exp?.type === 4) {
-        return `{{${fallbackProp.exp.content}}}`
+        return `{{${fallbackProp.exp.content}}}`;
       }
     }
-    return '-'
+    return "-";
   }
 
   get getHasPermissionDir(): DirectiveNode {
-    return findDir(this.bhe.child, 'hasPermission')!
+    return findDir(this.bhe.child, "hasPermission")!;
   }
 
   private helpFindNodeProps<T>(
@@ -78,21 +97,21 @@ export class BuilderHoistElementContextHelper {
   ): Array<T> {
     return t
       .map((prop) => {
-        const maybeDir = fn(child, prop, true)
+        const maybeDir = fn(child, prop, true);
         if (maybeDir) {
-          const { loc } = maybeDir
-          childMs.remove(loc.start.offset, loc.end.offset)
-          return maybeDir
+          const { loc } = maybeDir;
+          childMs.remove(loc.start.offset, loc.end.offset);
+          return maybeDir;
         }
       })
-      .filter(Boolean) as Array<T>
+      .filter(Boolean) as Array<T>;
   }
 
   static NomadizeProps(modifier: string) {
     if (isPermissionProps(modifier)) {
-      return hasPermissionPropsHelps[modifier]()
+      return hasPermissionPropsHelps[modifier]();
     }
   }
 
-  static SpaceSymbol = ' '
+  static SpaceSymbol = " ";
 }
--- index.ts
+++ index.ts
@@ -1,53 +1,58 @@
-import { baseParse, findDir, NodeTransform } from '@vue/compiler-core'
-import { compileTemplate, MagicString, parse, SFCDescriptor } from '@vue/compiler-sfc'
-import { first } from 'lodash'
-import { Plugin } from 'vite'
+import { baseParse, findDir, NodeTransform } from "@vue/compiler-core";
+import {
+  compileTemplate,
+  MagicString,
+  parse,
+  SFCDescriptor
+} from "@vue/compiler-sfc";
+import { first } from "lodash-es";
+import { Plugin } from "vite";
 
-import { BuilderHoistElement } from './builderHoistElement'
+import { BuilderHoistElement } from "./builderHoistElement";
 
 function replaceHoistNode(magicString: MagicString, descriptor: SFCDescriptor) {
   const onNode: NodeTransform = (node) => {
     return () => {
-      const { loc: templateLocStart } = descriptor.template!
+      const { loc: templateLocStart } = descriptor.template!;
       if (node.type === 1 /** NodeTypes.ELEMENT */) {
-        if (findDir(node, 'hasPermission')) {
-          const { loc: originNodeLoc } = node
+        if (findDir(node, "hasPermission")) {
+          const { loc: originNodeLoc } = node;
           const child = first(
             baseParse(originNodeLoc.source, {
               onError: () => {}
             }).children
-          )!
+          )!;
           if (child.type === 1) {
-            const tmpStartOffset = templateLocStart.start.offset
+            const tmpStartOffset = templateLocStart.start.offset;
             const prevChangeString = magicString.slice(
               tmpStartOffset + originNodeLoc.start.offset,
               tmpStartOffset + originNodeLoc.end.offset
-            )
-            const childMs = new MagicString(prevChangeString.toString())
-            const bhe = new BuilderHoistElement(child, childMs)
-            const { loc } = node
+            );
+            const childMs = new MagicString(prevChangeString.toString());
+            const bhe = new BuilderHoistElement(child, childMs);
+            const { loc } = node;
             magicString.overwrite(
               tmpStartOffset + loc.start.offset,
               tmpStartOffset + loc.end.offset,
               bhe.getBuilderNodeString()
-            )
+            );
           }
         }
       }
-    }
-  }
-  return onNode
+    };
+  };
+  return onNode;
 }
 
 export function replaceHoistNodePlugin(debug = false): Plugin {
   return {
-    name: 'vite-replace_hoist_node',
-    enforce: 'pre',
+    name: "vite-replace_hoist_node",
+    enforce: "pre",
     transform(source, id) {
-      if (id.endsWith('.vue') && source.includes('v-hasPermission')) {
-        const { descriptor } = parse(source)
+      if (id.endsWith(".vue") && source.includes("v-hasPermission")) {
+        const { descriptor } = parse(source);
         if (descriptor.template) {
-          const magicString = new MagicString(source)
+          const magicString = new MagicString(source);
           compileTemplate({
             filename: id,
             source: descriptor.template.content,
@@ -55,12 +60,12 @@ export function replaceHoistNodePlugin(debug = false): Plugin {
             compilerOptions: {
               nodeTransforms: [replaceHoistNode(magicString, descriptor)]
             }
-          })
-          debug && console.log('--------')
-          debug && console.log(magicString.toString())
-          return magicString.toString()
+          });
+          debug && console.log("--------");
+          debug && console.log(magicString.toString());
+          return magicString.toString();
         }
       }
     }
-  }
+  };
 }
